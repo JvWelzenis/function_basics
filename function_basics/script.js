579cc03aff6d6310b8e4521966b88f95f5f827dc
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-11-16 16:30:42 +0100

    Add files via upload

---
+++ function_basics/script.js
@@ -0,0 +1,13 @@
+function makingPizza (makingDough, addingSauce, addingToppings, oven) {
+    console.log(makingPizza);
+}
+console.log(makingPizza);
+console.log(makingPizza);
+console.log(makingPizza);
+// nee dit werkt niet want de functie is leeg. Er staat niets tussen de curly brackets.
+
+function makingSushi(cookRice, addVish, roll) {
+  console.log(makingSushi);
+}
+
+console.log(makingSushi, makingSushi, makingSushi, makingPizza, makingPizza);
